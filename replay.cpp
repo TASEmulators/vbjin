8c50ded66eba6960a0b55a90b63650968a8befd1
smelenchuk <smelenchuk@337e523f-d602-64bd-122f-1da801c1fc50>
2010-04-29 23:38:17 +0000

    Fix FPS in AVI output and in calculation for length display in replay dialogue.

--- replay.cpp
+++ replay.cpp
@@ -40,9 +40,9 @@ void Describe(HWND hwndDlg)
 
 	u32 num_frames = md.records.size();
 
-	double tempCount = num_frames / (33513982.0/6/355/263);
+	double tempCount = num_frames / (20000000.0/259/4/384);
 	int num_seconds = (int)tempCount;
-	int fraction = ((int)tempCount - num_seconds) * 100;
+	int fraction = ((int)(tempCount * 100)) % 100;
 	int seconds = num_seconds % 60;
 	int minutes = (num_seconds / 60) % 60;
 	int hours = (num_seconds / 60 / 60) % 60;
