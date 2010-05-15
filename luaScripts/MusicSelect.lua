7748cfbaa654c904cbf12f1b0fc36bfc0ad02b37
ugetab <ugetab@337e523f-d602-64bd-122f-1da801c1fc50>
2010-05-15 07:11:39 +0000

    Got the MusicSelect.lua code fully working with Teleroboxer, and made other general improvements.

--- luaScripts/MusicSelect.lua
+++ luaScripts/MusicSelect.lua
@@ -2,10 +2,18 @@
 --Page Up to increase, Page Down to decrease
 --
 --This is confirmed to work in the following games:
---VB Wario Land (From reset, any time after)
---Teleroboxer (From reset, any time after)
+--
+--Game Num List():
+--1 (Basic Testing)(VB Wario Land can use either, so it uses 2 for user convenience)
+--2 VB Wario Land (From reset, any time after)
+--2 Teleroboxer (From reset, any time after)
 
-local MemAddr = tonumber("050000F4", 16);
+local GameNum = 2;
+
+local InsertKeyMusSelect = 23;
+
+local MusReadAddr = tonumber("050000F6", 16);
+local MusWriteAddr = tonumber("050000F4", 16);
 local ButtonWasPressed;
 
 function MusicSelect()
@@ -16,24 +24,37 @@ if (not ButtonWasPressed) then
 
  if (kbinput.pageup) then
  ButtonWasPressed = 1;
- MusicValue = memory.readbyte(MemAddr);
+ MusicValue = memory.readbyte(MusReadAddr);
  MusicValue = tonumber(MusicValue) + 1;
- memory.writebyte(MemAddr,MusicValue);
  end;
  
  if (kbinput.pagedown) then
  ButtonWasPressed = 1;
- MusicValue = memory.readbyte(MemAddr);
+ MusicValue = memory.readbyte(MusReadAddr);
  MusicValue = tonumber(MusicValue) - 1;
- memory.writebyte(MemAddr,MusicValue);
+ end;
+
+-- An easily customized music selection key,
+-- if you want to jump from silence to a specific track.
+ if (kbinput.insert) then
+ ButtonWasPressed = 1;
+ MusicValue=InsertKeyMusSelect;
+ end;
+
+ if (ButtonWasPressed) then
+  if (GameNum == 2) then
+   memory.writebyte(tonumber("050000D0", 16),tonumber(MusicValue)); 
+  end;
+  memory.writebyte(MusWriteAddr,tonumber(MusicValue));
  end;
 
 end;
 
 -- Allows one to disable the boolean pretty easily
-ButtonWasPressed = (kbinput.pageup or kbinput.pagedown);
+ButtonWasPressed = (kbinput.pageup or kbinput.pagedown or kbinput.insert);
 
 end;
 
--- Sets the routine to run with the emulation
+-- Sets the routine to run with the emulation.
+-- Likely to make a good base script for doing cheat codes.
 emu.registerafter(MusicSelect);
