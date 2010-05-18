bdeb4776fa1f9cd497cbf173420bf42ccec49bf6
ugetab <ugetab@337e523f-d602-64bd-122f-1da801c1fc50>
2010-05-18 01:55:33 +0000

    Added 3D Tetris, Golf, and instructions for ripping Innsmouth Mansion, since I can't find a song init address for it.

--- trunk/luaScripts/MusicSelect.lua
+++ trunk/luaScripts/MusicSelect.lua
@@ -4,9 +4,11 @@
 --
 --This is confirmed to work in the following games:
 --
---Game Num List():
+--Game Num List:
 --1 (Basic Driver)(Some can use this or 2, so they use 2 for user convenience)
+--6 3D Tetris (From reset, any time after)
 --2 Galactic Pinball (From reset, any time after)
+--7 Golf (From reset, any time after)
 --2 Mario Clash (From reset, any time after)
 --4 Panic Bomber (From reset, any time after)
 --3 Red Alarm (From reset, any time after)
@@ -15,17 +17,28 @@
 --5 Vertical Force (From reset, any time after)
 
 local GameNum = 2;
+local InsertKeyMusSelect = 37;
 
 --Not Done:
---3D Tetris
---Golf
 --Jack Bros
 --Mario's Tennis (Broken)
 --Nester's Funky Bowling (Broken)
 --Virtual League Baseball (Broken)
 
-
-local InsertKeyMusSelect = 37;
+--Incompatible with LUA methods:
+--Innsmouth Mansion (J):
+--Edit the .VB file. No active music select address in RAM.
+--ECCA7 = Password Screen Music
+--ECCDF = Title Screen Music
+--ECD73 = menu start sound
+--18B30E07 = Silence
+--
+--Music Selection:
+--ECCA3 + (# * 4) = Music (# = 0x01 - 0x4F)
+--
+--I'd suggest replacing the password screen music(ECCA7),
+--setting ECD73 to 18B30E07,
+--and ripping from the point of going into the password screen.
 
 local MusReadAddr = tonumber("050000F6", 16);
 local MusWriteAddr = tonumber("050000F4", 16);
@@ -42,9 +55,19 @@ else
    StripBit80 = 1;
   else
    if (GameNum == 5) then
-   MusReadAddr = tonumber("05007DBD", 16);
-   MusWriteAddr = MusReadAddr;
-   StripBit80 = 1;
+    MusReadAddr = tonumber("05007DBD", 16);
+    MusWriteAddr = MusReadAddr;
+    StripBit80 = 1;
+   else
+    if (GameNum == 6) then
+     MusReadAddr = tonumber("05007631", 16);
+     MusWriteAddr = MusReadAddr;
+    else
+     if (GameNum == 7) then
+      MusReadAddr = tonumber("05004B41", 16);
+      MusWriteAddr = MusReadAddr;
+     end;
+    end;
    end;
  end;
 end;
@@ -58,33 +81,41 @@ local kbinput = input.get();
 if (not ButtonWasPressed) then
 
  if (kbinput.pageup) then
- ButtonWasPressed = 1;
- MusicValue = memory.readbyte(MusReadAddr);
- MusicValue = tonumber(MusicValue) + 1;
+  ButtonWasPressed = 1;
+  MusicValue = memory.readbyte(MusReadAddr);
+  MusicValue = tonumber(MusicValue) + 1;
  end;
  
  if (kbinput.pagedown) then
- ButtonWasPressed = 1;
- MusicValue = memory.readbyte(MusReadAddr);
- MusicValue = tonumber(MusicValue) - 1;
+  ButtonWasPressed = 1;
+  MusicValue = memory.readbyte(MusReadAddr);
+  MusicValue = tonumber(MusicValue) - 1;
  end;
 
 -- An easily customized music selection key,
 -- if you want to jump from silence to a specific track.
  if (kbinput.insert) then
- ButtonWasPressed = 1;
- MusicValue=InsertKeyMusSelect;
+  ButtonWasPressed = 1;
+  MusicValue=InsertKeyMusSelect;
  end;
 
  if (ButtonWasPressed) then
   if (GameNum == 2) then
-   memory.writebyte(tonumber("050000D0", 16),tonumber(MusicValue)); 
+    memory.writebyte(tonumber("050000D0", 16),tonumber(MusicValue)); 
+   else
+    if (GameNum == 3) then
+     memory.writebyte(tonumber("05000044", 16),1);
+    else
+     if (GameNum == 6) then
+      memory.writebyte(tonumber("05007630", 16),1);
+     else
+      if (GameNum == 7) then
+       memory.writebyte(tonumber("05004B40", 16),1);
+      end;
+     end;
+    end;
   end;
-  
-  if (GameNum == 3) then
-   memory.writebyte(tonumber("05000044", 16),1);
-  end;
-
+ 
   if (StripBit80 == 1) then
    if (tonumber(MusicValue) > 127) then
     MusicValue = (tonumber(MusicValue) - 128)
